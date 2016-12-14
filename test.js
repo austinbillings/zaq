b593ff21de91166a9c9a695ff754027575aaade4
Austin Billings <austinjosephbillings@gmail.com>
2016-12-14 16:38:33 -0500

    More improvements all around, new version 1.0.3

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var zaq = require('./zaq.js');
+var zaq = require('./zaq.min.js');
 
 var sampleJSON = {
   propName: {
@@ -10,14 +10,21 @@ var sampleJSON = {
   thirdThing: null
 };
 
-zaq.log('I\'m a log.');
+zaq.space('I\'m a .space()');
 zaq.win('I\'m a win!', sampleJSON);
 zaq.err('Oh no! I\'m an error. :(', sampleJSON);
 zaq.info('Some info here!', sampleJSON);
 zaq.warn('WATCH OUT FOR THAT SHIT.', sampleJSON);
-
+zaq.time('TIME TICKER', sampleJSON);
+zaq.space('I\'m a .space()');
 zaq.divider('I\'m a divider.');
+zaq.space('I\'m a .space()');
 zaq.divider('I\'m a CUSTOM DIVIDER!!!', '~*$');
+zaq.space('I\'m a .space()');
 zaq.json('I\'m JSON with a label', sampleJSON);
-
 zaq.obj(sampleJSON);
+zaq.win('I\'m a win!');
+zaq.err('Oh no! I\'m an error. :(');
+zaq.info('Some info here!');
+zaq.warn('WATCH OUT FOR THAT SHIT.');
+zaq.time('TIME TICKER');
--- zaq.js
+++ zaq.js
@@ -3,54 +3,66 @@ const _ = require('underscore');
 const chalk = require('chalk');
 
 let deprecated = (x) => {
-  zaq.log('Function [zaq.' + x + '()] is deprecated. Please consult your doctor.');
+  zaq.warn('Function ' + chalk.bold.yellow('[zaq.' + x + '()]') + ' is deprecated. Please consult your doctor.');
 }
 
-var zaq = {
+let zaq = {
   verbose: true,
   log: console.log,
-  obj: (obj, color = 'cyan') => {
-    let msg = chalk[color]('\n >>     ');
-    msg += (_.isString(obj) ? obj : zaq.pretty(obj)).split('\n').join('\n' + chalk.dim(' ::     '));
-    msg += '\n';
+  obj (obj, color = 'cyan') {
+    let msg = chalk[color]('\n >>    ');
+    msg += (_.isString(obj) ? obj : zaq.pretty(obj)).split('\n').join('\n' + chalk[color].dim(' ::    '));
+    //msg += chalk[color].dim('\n ' + zaq.nLines(40, '\''));
     return msg;
   },
-  win: (text, obj) => {
-    text = chalk.bold.green(' ✓ WIN:   ') + text;
+  win (text, obj) {
+    text = chalk.bold.green(' ✓ WIN:   ') + chalk.bold(text);
     return zaq.log(text + (obj ? zaq.obj(obj, 'green') : ''));
   },
-  err: (text, obj) => {
-    text = chalk.bold.red(' x ERR:   ') + text;
+  err (text, obj) {
+    text = chalk.bold.red(' x ERR:   ') + chalk.bold(text);
     return zaq.log(text + (obj ? zaq.obj(obj, 'red') : ''));
   },
-  warn: (text, obj) => {
-    text = chalk.bold.yellow(' # WARN:  ') + text;
+  warn (text, obj) {
+    text = chalk.bold.yellow(' # WARN:  ') + chalk.bold(text);
     return zaq.log(text + (obj ? zaq.obj(obj, 'yellow') : ''));
   },
-  info: (text, obj) => {
-    text = chalk.bold.blue(' → INFO:  ') + text;
+  info (text, obj) {
+    text = chalk.bold.blue(' → INFO:  ') + chalk.bold(text);
     return zaq.log(text + (obj ? zaq.obj(obj, 'blue') : ''));
   },
-  time: (text, obj) => {
-    text = chalk.bold.grey(' ♦ TIME:  ') + text;
-    return zaq.log(text + obj ? zaq.obj(obj, 'grey') : '');
+  time (text, obj) {
+    text = chalk.bold.grey(' ♦ TIME:  ') + chalk.bold(text);
+    return zaq.log(text + (obj ? zaq.obj(obj, 'grey') : ''));
   },
   pretty: (content) => JSON.stringify(content, null,'  '),
   space: (content) => zaq.log('\n' + content + '\n'),
-  nLines: (n, lines) => {
+  nLines (n, lines) {
     return Array(n).join(lines || '=')
   },
-  mini: (str) => {
+  mini (str) {
     return (_.isString(str) ? str : str.toString()).trim().substr(0, 100);
   },
-  divider: (text, lines) => {
+  divider (text, lines) {
     let lineCount = Math.floor((process.stdout.columns - (text.length + 1)) * (1 / (lines ? lines.length : 1)));
     return zaq.log(text + ' '+ zaq.nLines(lineCount, lines));
   },
-  json: (x, y) => deprecated('json'),
-  before: (obj) => deprecated('before'),
-  during: (obj) => deprecated('during'),
-  after: (obj) => deprecated('after')
+  json (x, y) {
+    zaq.log(zaq.obj(y ? y : x));
+    deprecated('json');
+  },
+  before (obj) {
+    zaq.log(zaq.obj(obj));
+    deprecated('before');
+  },
+  during (obj) {
+    zaq.log(zaq.obj(obj));
+    deprecated('during');
+  },
+  after (obj) {
+    zaq.log(zaq.obj(obj));
+    deprecated('after');
+  }
 };
 
 module.exports = zaq;
