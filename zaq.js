302e29cb7207365621cca3a02191e72e31292cba
Austin Billings <austin@austinbillings.com>
2018-02-21 02:24:15 -0500

    switch debug & flag styles to use cyan & magenta respectively

--- zaq.js
+++ zaq.js
@@ -69,7 +69,7 @@ const faqtory = (namespace = '') => {
   });
 
   zaq.flag = logType({
-    style: 'cyan',
+    style: 'magenta',
     prefix: '⌘ FLAG:',
     level: 'info'
   });
@@ -93,7 +93,7 @@ const faqtory = (namespace = '') => {
   });
 
   zaq.debug = logType({
-    style: 'magenta',
+    style: 'cyan',
     prefix: '◆ DEBUG:',
     level: 'debug'
   });
