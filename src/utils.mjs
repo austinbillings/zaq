c00bbaf30d104e48ef01b4c68cc02c62dd6f8fbd
austin <austin@austinbillings.com>
2024-10-18 12:02:58 -0400

    make zaq print out using util.inspect when object otherwise would be featureless, to capture errors and other non-plain object types

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,26 +1,29 @@
+import util from 'util';
 import chalk from 'chalk';
 
 const CONNECTORS = ['{','}','[',']'];
 
 export function joinBy (glue = '') {
-  return (...strings) => !Array.isArray(strings)
-    ? strings
-    : strings.filter(s => s).join(glue);
+    return (...strings) => !Array.isArray(strings)
+        ? strings
+        : strings.filter(s => s).join(glue);
 };
 
 export function nLines (n, line = '-') {
-  return Array(n).join(line)
+    return Array(n).join(line)
 };
 
 export function toString (content) {
-  return typeof content === 'undefined' ? 'undefined' : (JSON
-    .stringify(content, null, '  ') || '')
-    .split('\n')
-    .map(line => {
-      const trimmed = line.trim();
-      return trimmed.length && CONNECTORS.includes(trimmed)
-        ? chalk.dim(line)
-        : line;
-    })
-    .join('\n');
+    const result = typeof content === 'undefined' ? 'undefined' : (JSON
+        .stringify(content, null, '  ') || '')
+        .split('\n')
+        .map(line => {
+            const trimmed = line.trim();
+            return trimmed.length && CONNECTORS.includes(trimmed)
+                ? chalk.dim(line)
+                : line;
+        })
+        .join('\n');
+
+    return result === '{}' ? util.inspect(content) : result;
 }
